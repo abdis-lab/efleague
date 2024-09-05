8ec0914d56994edf34cbb169276e86161eb150cc
Abdi Kadir <abdisalamfresh@gmail.com>
2024-09-05 03:00:16 -0700

    Initial Commit

---
+++ src/main/java/com/abdisalam/efleague/EfleagueApplication.java
@@ -0,0 +1,13 @@
+package com.abdisalam.efleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfleagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfleagueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abdisalam/efleague/EfleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdisalam.efleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfleagueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
